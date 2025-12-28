a2bcedff972fdab3ff2e5a82348a57ab6f911b5f
Kravchenko Vladimir <kravchenko.vl@promail366.com>
2025-12-28 14:43:37 +0200

    fix1

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php echo "Hello World";
-
-
+<html>
+<head></head>
+<body>
+<?php echo "Привет " . @$_GET["fname"] . " " . @$_GET["lname"]; ?>
+<br>
+<?php var_dump($_GET); ?>
+<form>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" style="width: 200px"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" style="width: 200px"><br><br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
